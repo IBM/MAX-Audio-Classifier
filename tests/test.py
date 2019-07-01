2debd29d5764d1adf07c0b7d75caddc061cff84b
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2019-07-01 12:27:40 -0700

    License headers.

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2018-2019 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import pytest
 import requests
 
@@ -33,7 +48,7 @@ def test_metadata():
 
 def test_predict():
     model_endpoint = 'http://localhost:5000/model/predict'
-    file_path = 'assets/birds1.wav'
+    file_path = 'samples/birds1.wav'
 
     with open(file_path, 'rb') as file:
         file_form = {'audio': (file_path, file, 'audio/wav')}
@@ -53,7 +68,7 @@ def test_predict():
 def test_empty_filter():
 
     model_endpoint = 'http://localhost:5000/model/predict?filter='
-    file_path = 'assets/gunshots.wav'
+    file_path = 'samples/gunshots.wav'
 
     with open(file_path, 'rb') as file:
         file_form = {'audio': (file_path, file, 'audio/wav')}
@@ -74,7 +89,7 @@ def test_empty_filter():
 def test_multi_empty_filter():
 
     model_endpoint = 'http://localhost:5000/model/predict?filter=,,'
-    file_path = 'assets/gunshots.wav'
+    file_path = 'samples/gunshots.wav'
 
     with open(file_path, 'rb') as file:
         file_form = {'audio': (file_path, file, 'audio/wav')}
@@ -95,7 +110,7 @@ def test_multi_empty_filter():
 def test_filter():
 
     model_endpoint = 'http://localhost:5000/model/predict?filter=Cap%20gun'
-    file_path = 'assets/gunshots.wav'
+    file_path = 'samples/gunshots.wav'
 
     with open(file_path, 'rb') as file:
         file_form = {'audio': (file_path, file, 'audio/wav')}
@@ -116,7 +131,7 @@ def test_filter():
 def test_multi_filter():
 
     model_endpoint = 'http://localhost:5000/model/predict?filter=Clang,Ding'
-    file_path = 'assets/gunshots.wav'
+    file_path = 'samples/gunshots.wav'
 
     with open(file_path, 'rb') as file:
         file_form = {'audio': (file_path, file, 'audio/wav')}
